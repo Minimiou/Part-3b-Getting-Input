c12629697aefe4c447bc6bf8bbfb3a1517855e4b
Aya Kh <akhalife2003@gmail.com>
2020-09-23 12:44:38 -0400

    Add project files.

---
+++ Part 3bhGetting Input/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Part_3bhGetting_Input
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.Honeydew;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSubmit.Font = new System.Drawing.Font("LittleLordFontleroy", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSubmit.Location = new System.Drawing.Point(13, 96);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(234, 45);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Click Me!";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.Font = new System.Drawing.Font("LittleLordFontleroy", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruction.Location = new System.Drawing.Point(3, 21);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(261, 45);
+            this.lblInstruction.TabIndex = 1;
+            this.lblInstruction.Text = "Please enter your name:";
+            this.lblInstruction.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Font = new System.Drawing.Font("LittleLordFontleroy", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(249, 96);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(232, 45);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Message";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Salina", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(249, 19);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(232, 54);
+            this.txtInput.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(488, 169);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "MainForm";
+            this.Text = "Getting Input";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
